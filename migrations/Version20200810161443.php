9b7d9cd6a85e58ecb4a65154116df15a201e2cc4
Lewis Larsen <lewislrsn@gmail.com>
2020-08-11 22:44:10 +0100

    updated description

--- migrations/Version20200810161443.php
+++ migrations/Version20200810161443.php
@@ -23,7 +23,7 @@ final class Version20200810161443 extends AbstractMigration
         $this->addSql('CREATE TABLE heather_gallery (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, visible BOOLEAN NOT NULL DEFAULT 1, file_url VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL)');
         $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('5731d06ce812ahd728.png', 'Heather on a baseball field.')");
         $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('3240916424.jpg', 'Heather standing on the grass.')");
-        $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('3150433295.jpg', 'last school photo.')");
+        $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('3150433295.jpg', 'The last school photo for Heather, she was 12.')");
         $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('2395985929.jpg', 'Heather in the TV movie Surviving.')");
         $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('6023659288.jpg', 'Heather holding a gift during a photoshoot.')");
         $this->addSql("INSERT INTO heather_gallery (file_url, description) VALUES ('1315592550.jpg', 'Heather wearing a red dress.')");
